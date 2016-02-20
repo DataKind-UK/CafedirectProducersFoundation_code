5b8a1c8b284775d3bac0ad732f81e455f0661891
Gianfranco Cecconi <giacecco@dico.im>
2016-02-20 16:00:37 +0000

    Better

--- columns_transformation.R
+++ columns_transformation.R
@@ -1,16 +1,17 @@
-columns_transformation <- function (source_table, column_name_prefix, max_options) {
-    temp <- source_table
-    for (row_no in seq(1, nrow(temp))) {
+columns_transformation <- function (source_table, column_name_prefix) {
+    target_columns <- names(source_table)[grepl(column_name_prefix, names(source_table))]
+    max_options <- max(sapply(target_columns, function (x) { return(as.numeric(substr(x, nchar(column_name_prefix) + 1, nchar(x)))) }))
+    for (row_no in seq(1, nrow(source_table))) {
         responses <- table(data.matrix(country[row_no, sapply(seq(1, max_options), function (x) { return(paste0(column_name_prefix, as.character(x), collapse = "")) }) ]))
         for (x in seq(1, max_options)) {
-            temp[row_no, paste0(column_name_prefix, as.character(x), collapse = "")] <- ifelse(sum(names(responses) == as.character(x)) > 0, 1, -1)
+            source_table[row_no, paste0(column_name_prefix, as.character(x), collapse = "")] <- ifelse(sum(names(responses) == as.character(x)) > 0, 1, -1)
         }
     }
-    return(temp)    
+    return(source_table)    
 }
 
 # EXAMPLE
 # Read the original...
 uganda <- read.csv("impact/UgandaClean.mapped.csv")
 # ... and transform
-uganda_new <- columns_transformation(uganda, "Q145.MostValuableSources.O", 14)
+uganda_new <- columns_transformation(uganda, "Q145.MostValuableSources.O")
